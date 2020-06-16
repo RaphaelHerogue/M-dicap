4f83cbc56f033d9359f15eaf8824af7f69295a5f
ROBERT Pierre-Alexandre <pierrealexrobert21@gmail.com>
2020-06-16 16:18:09 +0200

    navigation bt

--- www/inc/nav.php
+++ www/inc/nav.php
@@ -1,4 +1,43 @@
 <!-- La barre de navigation -->
+
+<nav>
+
+<!-- BLOCK 1 !!!!! -->
+
+  <!-- Cercle de personnes -->
+<div class="row">
+
+
+  <svg class="bi bi-person-circle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z"/>
+    <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+    <path fill-rule="evenodd" d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"/>
+  </svg>
+
+  </div>
+
+  <!-- Barre de menu -->
+
+<div class="low">
+  <svg class="bi bi-justify" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+  </svg>
+</div>
+
+
+  <!--  Logo -->
+
+<div class="row3">
+  <img src="../images/logo.svg" alt="">
+
+</div>
+
+<!-- INFORMATION : box-shaddow -->
+
+<!-- fIN BLOCK 1 -->
+
+<!-- BLOCK 2 -->
+
 <div id="lancebar">
   <div class="barredenav">
     <span></span>
@@ -8,9 +47,31 @@
   </div>
 
   <ul>
-    <li></li>
-    <li></li>
-    <li></li>
+    <li>L'entreprise</li>
+    <li>Information</li>
+
   </ul>
 
 </div>
+
+
+<!-- BLOCK 3 -->
+
+
+
+<button class="button button1">Connexion</button>
+<button class="button button2">Inscription</button>
+
+
+<!-- Retour-->
+
+<svg class="bi bi-box-arrow-in-left" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M7.854 11.354a.5.5 0 0 0 0-.708L5.207 8l2.647-2.646a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708 0z"/>
+  <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0 0 1h9A.5.5 0 0 0 15 8z"/>
+  <path fill-rule="evenodd" d="M2.5 14.5A1.5 1.5 0 0 1 1 13V3a1.5 1.5 0 0 1 1.5-1.5h8A1.5 1.5 0 0 1 12 3v1.5a.5.5 0 0 1-1 0V3a.5.5 0 0 0-.5-.5h-8A.5.5 0 0 0 2 3v10a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-1.5a.5.5 0 0 1 1 0V13a1.5 1.5 0 0 1-1.5 1.5h-8z"/>
+</svg>
+
+
+
+
+</nav>
