1b414f732f1ee67e980ca41dd1821e95ddd56c03
ROBERT Pierre-Alexandre <pierrealexrobert21@gmail.com>
2020-06-16 14:28:24 +0200

    nav

---
+++ www/inc/nav.php
@@ -0,0 +1,16 @@
+<!-- La barre de navigation -->
+<div id="lancebar">
+  <div class="barredenav">
+    <span></span>
+    <span></span>
+    <span></span>
+
+  </div>
+
+  <ul>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+
+</div>
