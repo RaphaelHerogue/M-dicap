471a0467aa9759844478daccf0e6a7da98b5af28
ROBERT Pierre-Alexandre <pierrealexrobert21@gmail.com>
2020-06-19 09:42:15 +0200

    changes styles nav header

--- www/inc/header.php
+++ www/inc/header.php
@@ -2,16 +2,16 @@
   <nav class="navbar navbar-expand-md navbar-light bg-light sidebarNavigation" data-sidebarClass="navbar-light bg-light">
 
   <div class="container-fluid">
-    <div class="row align-items-center justify-content-center">
+    <div class="row align-items-center">
 
 
-      <div class="col-3 text-center align-content-center d-md-none d-lg-none" id="menu">
+      <div class="col-3 text-center align-content-center" id="menu">
         <button class="navbar-toggler leftNavbarToggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
             aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
             <img src="../images/icons/icone-menu.svg" alt="menu">
         </button>
       </div>
-      <div class="col" id="logo">
+      <div class="col-9" id="logo">
         <a class="navbar-brand" href="index.php"><img src="../images/logo.svg" alt="logo medicap"></a>
       </div>
     </div>
--- www/index.php
+++ www/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/styles.css">
     <link rel="stylesheet" href="/css/bootstrap.min.css">
-  <script type="text/javascript" src="js/scriptNav.js">
+  <script type="text/javascript" src="js/script.js">
 
   </script>
     <title>Médicap</title>
