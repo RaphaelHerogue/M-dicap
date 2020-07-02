0f886efdd59833719adc596ffcada6ad52883f47
ROBERT Pierre-Alexandre <pierrealexrobert21@gmail.com>
2020-07-02 08:48:26 +0200

    mise en page du formulaire

--- www/js/control.js
+++ www/js/control.js
@@ -1,39 +1,55 @@
 $(document).ready(function(){
+
   $("#formulForm").validate({
+
     rules: {
+
       lastname: {
         required: true,
         minlength: 2
       },
+
       firstname: {
         required: true,
       },
+
       email: {
         required: true,
         email: true,
       },
+
       tel: {
         required: true,
         minlength: 10,
       },
+
       agree:"required"
     },
+
     messages: {
+
       lastname: {
+
         required: "Entrer votre Nom de famille",
         minlength: "Veuillez entrer minimum 2 lettres"
       },
+
       firstname: {
         required: "Entrer votre Prenom",
       },
+
       email: {
           required: "Entrer votre email",
           email:"Veuillez respecter le format"
       },
+
       phone: {
         required:"Entrer votre Numero de tel",
       },
+
       agree: "Vous devez accepter que vos informations nous soit transmises"
     },
+
   });
+
 });
