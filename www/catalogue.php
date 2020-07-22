b178aeda0a569a807e42f4584c4e0f4a1e58c97a
Jordan Gape <jordangapee@gmail.com>
2020-07-22 16:57:15 +0200

    ajout image du catalogue

---
+++ www/catalogue.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="images/icons/favIcon.svg" />
+  <link rel="stylesheet" href="/css/styles.css">
+  <link rel="stylesheet" href="/css/bootstrap.min.css">
+  <script type="text/javascript" src="js/scriptNav.js"></script>
+
+  <title>Médicap</title>
+</head>
+<body>
+  <?php
+  include "inc/header.php";
+  ?>
+
+  <main>
+
+    <h1 class="text-center text-primary"></h1>
+      <div class="container d-flex flex-wrap" id="catalogue">
+        <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-between my-2 py-2 shadowBloc rounded col-2">
+          <img src="../images/lit-medical-xxl.png" alt="lit medical" class="rounded">
+        </div>
+        <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-between my-2 py-2 shadowBloc rounded col-2">
+          <img src="../images/lit-medical-xxl.png" alt="lit medical" class="rounded">
+        </div>
+        <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-between my-2 py-2 shadowBloc rounded col-2">
+          <img src="../images/lit-medical-xxl.png" alt="lit medical" class="rounded">
+        </div>
+        <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-between my-2 py-2 shadowBloc rounded col-2">
+          <img src="../images/lit-medical-xxl.png" alt="lit medical" class="rounded">
+        </div>
+        <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-between my-2 py-2 shadowBloc rounded col-2">
+          <img src="../images/lit-pliable.png" alt="lit pliable" class="rounded">
+        </div>
+        <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-between my-2 py-2 shadowBloc rounded col-2">
+          <img src="../images/lit-pliable.png" alt="lit pliable" class="rounded">
+        </div>
+        <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-between my-2 py-2 shadowBloc rounded col-2">
+          <img src="../images/lit-pliable.png" alt="lit pliable" class="rounded">
+        </div>
+        <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-between my-2 py-2 shadowBloc rounded col-2">
+          <img src="../images/lit-pliable.png" alt="lit pliable" class="rounded">
+        </div>
+        <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-between my-2 py-2 shadowBloc rounded col-2">
+          <img src="../images/lit-medicalise.png" alt="lit medicalise" class="rounded">
+        </div>
+        <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-between my-2 py-2 shadowBloc rounded col-2">
+          <img src="../images/lit-medicalise.png" alt="lit medicalise" class="rounded">
+        </div>
+        <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-between my-2 py-2 shadowBloc rounded col-2">
+          <img src="../images/lit-medicalise.png" alt="lit medicalise" class="rounded">
+        </div>
+        <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-between my-2 py-2 shadowBloc rounded col-2">
+          <img src="../images/lit-medicalise.png" alt="lit medicalise" class="rounded">
+        </div>
+        <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-between my-2 py-2 shadowBloc rounded col-2">
+          <img src="../images/litmedical.png" alt="lit medical" class="rounded">
+        </div>
+        <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-between my-2 py-2 shadowBloc rounded col-2">
+          <img src="../images/litmedical.png" alt="lit medical" class="rounded">
+        </div>
+        <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-between my-2 py-2 shadowBloc rounded col-2">
+          <img src="../images/litmedical.png" alt="lit medical" class="rounded">
+        </div>
+        <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-between my-2 py-2 shadowBloc rounded col-2">
+          <img src="../images/litmedical.png" alt="lit medical" class="rounded">
+        </div>
+      </div>
+    </main>
+    <?php
+    include "inc/footer.php";
+    ?>
+  </body>
+  </html>
--- www/inc/carte.php
+++ www/inc/carte.php
@@ -93,3 +93,117 @@
     </div>
   </div>
 </div>
+
+<div class="container my-5 py-5 shadowBloc rounded catalogue">
+  <h2 class="card-title text-primary text-center mb-5">Catalogue</h5>
+  <div class="card flex-row d-flex align-items-center">
+    <div class="col-6 col-md-6">
+      <img src="../images/lit-medical-xxl.svg" alt="lit xxl" "class= mx-auto icones-activity rounded">
+    <div class="col-6 col-md-6">
+      <img src="../images/Lit-medicalise.svg" alt="lit medicalise" "class= mx-auto icones-activity rounded">
+    </div>
+  </div>
+</div>
+</div>
+
+
+<!-- cartes page detailsproduct -->
+<div id="detailsProduct">
+  <div class="container my-5 py-5 shadowBloc rounded">
+    <div class="container d-flex justify-content-between align-items-center">
+      <label class="col-3 m-0 text-primary" for="size">Largeur</label>
+      <select class="col-6 rounded btn btn-outline-secondary" id="size" name="size">
+        <option selected>Choisir</option>
+        <option value="1">90 cm</option>
+        <option value="2">140 cm</option>
+        <option value="3">210 cm</option>
+      </select>
+    </div>
+    <div class="container mt-5 d-flex justify-content-between align-items-center">
+      <label class="col-3 m-0 text-primary" for="color">Couleur</label>
+      <select class="col-6 rounded btn btn-outline-secondary" id="color" name="color">
+        <option selected>Choisir</option>
+        <option value="1">Hêtre de Bavière</option>
+        <option value="2">Noir</option>
+        <option value="3">blanc</option>
+      </select>
+    </div>
+  </div>
+
+  <div class="container my-5 p-5 shadowBloc rounded">
+    <div class="row align-items-center">
+      <div class="col-12 col-md-6 d-flex flex-column">
+        <h2 class="text-primary">1490,00€</h2>
+        <span id="infoPrix">Dont 7,00 € d'écotaxe</span>
+        <span>Payer en 3x sans frais</span>
+        <span>3 mensualités: 496,67 € + 496,67 € </span>
+      </div>
+      <div class="pt-4 col-6 col-md-3 d-flex justify-content-center align-items-center">
+        <button class="btn" type="button" name="button">
+          <img class="icones" src="images/icons/moins.svg" alt="icones moins">
+        </button>
+        <span>1</span>
+        <button class="btn" type="button" name="button">
+          <img class="icones" src="images/icons/plus.svg" alt="icones plus">
+        </button>
+      </div>
+      <div class="pt-4 col-6 col-md-3 text-center">
+        <button class="rounded btn btn-outline-secondary" type="button" name="button">Ajouter au panier
+          <img class="icones" src="images/icons/icon-shop-vert.svg" alt="panier">
+        </button>
+      </div>
+    </div>
+    <a class="d-block text-right" href="">Demande de devis </a>
+  </div>
+
+  <div class="container my-5 p-5 shadowBloc rounded">
+    <div id="accordion">
+      <div class="card">
+        <div class="border-bottom" id="headingOne">
+          <h5 class="p-2 mb-0 d-flex justify-content-between">
+            <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              Description
+            </button>
+            <img class="icones" src="../images/icons/icone-fleche.svg" alt="">
+          </h5>
+        </div>
+        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+          <div class="card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="border-bottom" id="headingTwo">
+          <h5 class="p-2 mb-0 d-flex justify-content-between">
+            <button class="btn btn-link" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              Livraison et Retours
+            </button>
+              <img class="icones" src="../images/icons/icone-fleche.svg" alt="">
+          </h5>
+        </div>
+        <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordion">
+          <div class="card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <div class="border-bottom" id="headingThree">
+          <h5 class="p-2 mb-0 d-flex justify-content-between">
+            <button class="btn btn-link" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              Avis (34)
+            </button>
+            <img class="icones" src="../images/icons/icone-fleche.svg" alt="">
+          </h5>
+        </div>
+        <div id="collapseThree" class="collapse show" aria-labelledby="headingThree" data-parent="#accordion">
+          <div class="card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- fin des cartes page detailsproduct -->
